a99a076982c3d3cb25da974cd27d1b1278af37b5
Oleg Korneev <olvikorneev@gmail.com>
2022-07-05 16:13:51 +0300

    Chenged sortTasks variable to currentTask variable in TimerContainer. Fixed the dispay of  pomodoroCount and taskNumber

--- src/components/AppPage/Timer/TimerContainer.tsx
+++ src/components/AppPage/Timer/TimerContainer.tsx
@@ -26,8 +26,6 @@ export function TimerContainer() {
   const [breakSeconds, setBreakSeconds] = useState(0);
   const [isChangedMode, setIsChangedMode] = useState(false);
 
-
-
   const currentDate = new Date();
   const currentDateStringYYYYMMDD = getDateStringYYYYMMDD(currentDate);
 
@@ -36,133 +34,26 @@ export function TimerContainer() {
 
   // Есть ли задачи для выполнения?
   const isTasks = tasks.length !== 0;
-
   // Сортировка массива дел старое сверху
-  const currentTask = [...tasks].sort((prev, current) => prev.id - current.id)[0];
-
+  const sortTasks = [...tasks].sort((prev, current) => prev.id - current.id);
+  const currentTask = sortTasks[0];
+  // Если задача поменялась, в стэйт нужны свежие данные
   useEffect(() => {
     setSecondsLeft(isTasks ? currentTask.time : settingsInfo.taskTime);
   }, [tasks])
-
-  //TODO заменить все sortTask на currentTask
-  const sortTasks = [...tasks].sort((prev, current) => prev.id - current.id);
-  // Если есть задачи, берем время на их помидор
-  // setSecondsLeft(isTasks ? sortTasks[0].time : 0);
   // Получаем название задачи из массива
-  const taskName = isTasks ? sortTasks[0].name : 'Нет задач';
+  const taskName = isTasks ? currentTask.name : 'Нет задач';
+
   // Ф-ция увеличения значения счётчика с шагом в 1 минуту
   function handleAddTime() {
-    dispatch(increaseTime(sortTasks[0].id));
-    // setSecondsLeft((prevState) => prevState + 60);
+    dispatch(increaseTime(currentTask.id));
   }
 
-  // TODO Получаем из stat статистику текущего дня
+  // Получаем из stat статистику текущего дня
   const [currentDateStat] = stats.filter((item) => item.date === currentDateStringYYYYMMDD);
   const isCurrentDateStat = currentDateStat !== undefined;
-  // console.log('date', isCurrentDateStat);
-  let pomodoroCount = isCurrentDateStat ? currentDateStat.pomodoroCount : 0;
-  let taskNumber = isCurrentDateStat ? currentDateStat.taskCount : 0;
-  // Если есть задачи, текущее значение увеличиваем на 1
-  pomodoroCount = isTasks ? pomodoroCount + 1 : pomodoroCount;
-  taskNumber = isTasks ? taskNumber + 1 : taskNumber;
-
-  // console.log(secondsLeft);
-  // console.log(isTasks);
-  // const currentTaskTime = currentTask ? currentTask.time : 0;
-  // console.log(currentTaskTime);
-  // console.log(secondsLeft);
-
-  // useEffect(() => {
-  //   function switchMode() {
-  //     let nextTime = 0;
-  //     if(states.isStarted) {
-  //       nextTime = settingsInfo.shortBreakTime;
-  //       if(isCurrentDateStat && (currentDateStat.lastLongBreakPomodoroCount >= settingsInfo.longBreakCycle)) {
-  //         nextTime = settingsInfo.longBreakTime;
-  //         dispatch(cleanLastLongBreakPomodoroCount(currentDateStringYYYYMMDD));
-  //       } else {
-  //         dispatch(increaseLastLongBreakPomodoroCount(currentDateStringYYYYMMDD));
-  //       }
-  //       dispatch(startTimer(false))
-  //       dispatch(breakTimer(true));
-  //       setSecondsLeft(nextTime);
-  //     } else {
-  //       nextTime = settingsInfo.taskTime;
-  //       dispatch(startTimer(true))
-  //       dispatch(breakTimer(false));
-  //     }
-  //
-  //     setSecondsLeft(nextTime);
-  //   }
-  //
-  //  const intervalID = setInterval(() => {
-  //    if (!states.isStarted || !states.isBreak) {
-  //      return
-  //    }
-  //
-  //    if(states.onPause || states.onBreakPause) {
-  //      return
-  //    }
-  //
-  //    if(secondsLeft === 0) {
-  //      return switchMode();
-  //    }
-  //    setSecondsLeft((seconds) => seconds - 1);
-  //   }, 1000)
-  //
-  //   return () => clearInterval(intervalID);
-  // }, [states, stats, secondsLeft, settingsInfo, isCurrentDateStat, currentDateStat])
-  //
-  // console.log(secondsLeft);
-
-
-
-
-
-
-
-
-
-
-  // Логика работы таймера
-  // В states состояние App
-
-
-  // Запустк Таймера
-  // useEffect(() => {
-  //   let timerId: NodeJS.Timer;
-  //   if(states.isStarted) {
-  //     timerId = setInterval(() => {
-  //       setTime((prevTime) => prevTime + 1)
-  //     }, 1000)
-  //   }
-  //   return () => clearInterval(timerId);
-  // }, [states.isStarted])
-
-  // // Запуск Таймера Паузы
-  // useEffect(() => {
-  //   let timerId: NodeJS.Timer;
-  //   if(states.onPause) {
-  //     timerId = setInterval(() => {
-  //       setPauseTime((prevPauseTime) => prevPauseTime + 1)
-  //     }, 1000)
-  //   }
-  //   return () => clearInterval(timerId);
-  // }, [states.onPause]);
-  // // console.log(pauseTime);
-
-  // // Запуск Таймера Перерыва
-  // useEffect(() => {
-  //   let timerId: NodeJS.Timer;
-  //   if(states.isBreak) {
-  //     timerId = setInterval(() => {
-  //       setBreakTime((prevBreakTime) => prevBreakTime + 1)
-  //     }, 1000)
-  //   }
-  //   return () => clearInterval(timerId);
-  // }, [states.isBreak]);
-  // // console.log(breakTime);
-
+  let pomodoroCount = isCurrentDateStat && isTasks ? currentDateStat.pomodoroCount + 1 : 0;
+  let taskNumber = isCurrentDateStat && isTasks? currentDateStat.taskCount +1 : 0;
 
 
   // Определение типа окна исходя из states
@@ -215,14 +106,14 @@ export function TimerContainer() {
           // Увеиличить StopCount
         const newStat = {
           ...statDataTemplate,
-          timerTime: sortTasks[0].time - secondsLeft + pauseSeconds,
+          timerTime: currentTask.time - secondsLeft + pauseSeconds,
           pauseTime: pauseSeconds,
           stopCount: 1,
         }
         // Записываем статистику
         dispatch(changeStat(newStat));
         // Установить время secondsLeft = время задачи
-        setSecondsLeft(sortTasks[0].time);
+        setSecondsLeft(currentTask.time);
         // Обнулить pauseSeconds
         setPauseSeconds(0);
         // Установить таймер в false
@@ -265,16 +156,16 @@ export function TimerContainer() {
       setIsChangedMode(false);
       if(states.isStarted) {
         //Если ещё несколько задач или помидоров
-        if(sortTasks.length > 1 || sortTasks[0].count > 1) {
+        if(sortTasks.length > 1 || currentTask.count > 1) {
           // Записать статистику
           console.log("Запись статистики. Есть ещё помидоры");
           const newStat = {
             ...statDataTemplate,
-            timerTime: sortTasks[0].time - secondsLeft + pauseSeconds,
-            pomodoroTime: sortTasks[0].time - secondsLeft,
+            timerTime: currentTask.time - secondsLeft + pauseSeconds,
+            pomodoroTime: currentTask.time - secondsLeft,
             pauseTime: pauseSeconds,
             pomodoroCount: 1,
-            taskCount: sortTasks[0].count === 1 ? 1 : 0 ,
+            taskCount: currentTask.count === 1 ? 1 : 0 ,
             lastLongBreakPomodoroCount: 1,
           }
           console.log(newStat);
@@ -293,11 +184,11 @@ export function TimerContainer() {
           console.log("Запись статистики. Нет помидоров")
           const newStat = {
             ...statDataTemplate,
-            timerTime: sortTasks[0].time - secondsLeft + pauseSeconds,
-            pomodoroTime: sortTasks[0].time - secondsLeft,
+            timerTime: currentTask.time - secondsLeft + pauseSeconds,
+            pomodoroTime: currentTask.time - secondsLeft,
             pauseTime: pauseSeconds,
             pomodoroCount: 1,
-            taskCount: sortTasks[0].count === 1 ? 1 : 0 ,
+            taskCount: currentTask.count === 1 ? 1 : 0 ,
             // lastLongBreakPomodoroCount: 1,
           }
           // Записываем статистику
@@ -320,7 +211,7 @@ export function TimerContainer() {
           // lastLongBreakPomodoroCount: 1,
         }
         dispatch(changeStat(newStat));
-        setSecondsLeft(sortTasks[0].time);
+        setSecondsLeft(currentTask.time);
         setBreakSeconds(0);
         dispatch(startTimer(true));
       }
@@ -372,10 +263,10 @@ export function TimerContainer() {
 
   function setFinishedPomodoro() {
     // console.log('function');
-    if (sortTasks[0].count > 1) {
-      dispatch(decreaseCount(sortTasks[0].id));
+    if (currentTask.count > 1) {
+      dispatch(decreaseCount(currentTask.id));
     } else {
-      dispatch(removeTask(sortTasks[0].id));
+      dispatch(removeTask(currentTask.id));
     }
   }
 
